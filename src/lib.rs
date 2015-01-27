860945b5be7522dc323d831b779220f4743bd881
Andrew Clarkson <andrew@clarkson.mn>
2015-01-26 22:15:25 -0600

    begin adding a token iterator

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,11 @@
 
 #![crate_id="yaml"]
 
-struct Scanner<'a> {
+use std::iter::Iterator;
+use std::option::Option;
+
+
+pub struct Scanner<'a> {
     reader: Box<Reader + 'a>,
 }
 
@@ -13,15 +17,20 @@ impl <'a>Scanner<'a> {
     pub fn new<T: Reader + 'a>(reader: Box<T>) -> Scanner<'a> {
         Scanner { reader: reader } 
     }
-
-    pub fn scan(&mut self) {
-
-        println!("{:?}", self.reader.read_byte().unwrap());
-    }
-
 }
 
+pub enum Token {
+    Character,
+}
 
+impl <'a>Iterator for Scanner<'a> {
+    
+    // TODO: add Item
+
+    pub fn next(&mut self) -> Option<Token> {
+        Option::Some(Token::Character)
+    }
+}
 
 
 mod test {
@@ -36,6 +45,8 @@ mod test {
         let stream = "---";
         let mut reader = MemReader::new(stream.bytes().collect());
         let mut scanner = Scanner::new(Box::new(reader));
-        scanner.scan();
+        for token in scanner.iter() {
+            println!("{}", token);           
+        }
     }
 }
