39fd44527c1071ca94cd4926badbeb75d9e0b47e
Andrew Clarkson <andrew@clarkson.mn>
2015-01-27 08:42:55 -0600

    Implement a token iterator

--- src/lib.rs
+++ src/lib.rs
@@ -19,15 +19,17 @@ impl <'a>Scanner<'a> {
     }
 }
 
+#[derive(PartialEq, Copy, Show)]
 pub enum Token {
     Character,
+    Other,
 }
 
 impl <'a>Iterator for Scanner<'a> {
     
-    // TODO: add Item
+    type Item = Token;
 
-    pub fn next(&mut self) -> Option<Token> {
+    fn next(&mut self) -> Option<Token> {
         Option::Some(Token::Character)
     }
 }
@@ -38,15 +40,15 @@ mod test {
     use std::io::MemReader;
     use std::vec;
 
-    use super::Scanner;
+    use super::{Scanner, Token};
 
     #[test]
     fn test_scanner() {
         let stream = "---";
         let mut reader = MemReader::new(stream.bytes().collect());
         let mut scanner = Scanner::new(Box::new(reader));
-        for token in scanner.iter() {
-            println!("{}", token);           
+        for token in scanner.take(5) {
+            assert!(token == Token::Character);           
         }
     }
 }
