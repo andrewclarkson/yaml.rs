27ee3cd5c17d9e0053d0d05a3c006efa90f12fa7
Andrew Clarkson <andrew@clarkson.mn>
2015-01-27 12:13:43 -0600

    All indicator tokens + line breaks

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,21 @@
 use std::iter::Iterator;
 use std::option::Option;
 
-
-pub struct Scanner<'a> {
+/// A Yaml Tokenizer
+///
+/// A tokenizer takes a byte stream and produces a stream of Tokens
+/// This shelters the parser from the complexity of dealing with raw bytes.
+///
+pub struct Tokenizer<'a> {
     reader: Box<Reader + 'a>,
     done: bool,
 }
 
-impl <'a>Scanner<'a> {
-    pub fn new<T: Reader + 'a>(reader: Box<T>) -> Scanner<'a> {
-        Scanner { reader: reader, done: false } 
+impl <'a>Tokenizer<'a> {
+    
+    /// Creates a new Tokenizer from a Reader
+    pub fn new<T: Reader + 'a>(reader: Box<T>) -> Tokenizer<'a> {
+        Tokenizer { reader: reader, done: false } 
     }
 
     pub fn get_next_token(&mut self) -> Option<Token> {
@@ -31,8 +37,28 @@ impl <'a>Scanner<'a> {
             },
             Ok(byte) => {
                 match byte as char {
+                    
+                    // Indicators:
                     '[' => Token::SequenceStart,
-                    _ => Token::Other
+                    ']' => Token::SequenceEnd,
+                    '{' => Token::MappingStart,
+                    '}' => Token::MappingEnd,
+                    // TODO: peek ahead to check for a document opening
+                    '-' => Token::SequenceEntry,
+                    ':' => Token::MappingSeparator,
+                    ',' => Token::CollectionSeparator,
+                    '?' => Token::ComplexKey,
+                    '!' => Token::Tag,
+                    '&' => Token::Anchor,
+                    '*' => Token::Alias,
+                    '|' => Token::Literal,
+                    '>' => Token::Folded,
+                    '\'' => Token::SingleQuote,
+                    '"' => Token::DoubleQuote,
+                    '#' => Token::Comment,
+                    '%' => Token::Directive,
+                    '@' | '`' => Token::Reserved,
+                    _ => Token::Other,
                 }
             }
         };
@@ -41,14 +67,103 @@ impl <'a>Scanner<'a> {
     }
 }
 
-#[derive(PartialEq, Copy, Show)]
+/// All the types of tokens
+///
+/// Describes all of the token types described by the [Yaml 1.0 Spec](http://yaml.org/spec/1.0/)
+///
+#[derive(PartialEq, Copy, Debug)]
 pub enum Token {
+
+    /// `[`: the start of a _flow_ sequence
+    /// `c-sequence-start` in the spec
     SequenceStart,
+
+    /// `]`: the end of a _flow_ sequence
+    /// `c-sequence-end` in the spec
+    SequenceEnd,
+
+    /// `{`: the end of a _flow_ mapping
+    /// `c-mapping-start` in the spec
+    MappingStart,
+
+    /// `}`: the end of a _flow_ mapping
+    /// `c-mapping-end` in the spec
+    MappingEnd,
+
+    /// `-`: indicates an entry in a sequence
+    /// `c-sequence-entry` in the spec
+    SequenceEntry,
+
+    /// `:`: separates key from value in a mapping
+    /// `c-mapping-entry` in the spec
+    MappingSeparator,
+    
+    /// `,`: separates entries in _flow_ collections (mapping or sequence)
+    /// `c-collect-entry` in the spec
+    CollectionSeparator,
+
+    /// `?`: indicates a complex key (e.g a non-scalar key)
+    /// `c-complex-key` in the spec
+    ComplexKey,
+
+    /// `!`: indicates a tag property
+    /// `c-tag` in the spec
+    Tag,
+
+    /// `&`: indicates an anchor property (e.g setting a variable)
+    /// `c-anchor` in the spec
+    Anchor,
+
+    /// `*`: indicates an anchor property (e.g using a variable)
+    /// `c-alias` in the spec
+    Alias,
+
+    /// `|`: indicates a literal value (not to be escaped)
+    /// `c-literal` in the spec
+    Literal,
+
+    /// `>`: indicates a folded value (ignore line breaks)
+    /// `c-folded` in the spec
+    Folded,
+
+    /// `'`: a single quoted scalar
+    /// `c-single-quote` in the spec
+    SingleQuote,
+    
+    /// `"`: a double quoted scalar
+    /// `c-double-quote` in the spec
+    DoubleQuote,
+
+    /// `#`: a comment
+    /// `c-throwaway`
+    Comment,
+
+    /// `%`: a directive (tags for the YAML parser)
+    /// `c-directive` in the spec
+    Directive,
+
+    /// `@ | \``: reserved for future use?
+    /// `c-reserved` in the spec
+    Reserved,
+
+    /// ``: a line break with no specific purpose
+    LineBreakGeneric,
+
+    /// ``
+    LineBreak(LineBreak),
+
     Other, // A temporary addition for unimplemented tokens
     Eof,
 }
 
-impl <'a>Iterator for Scanner<'a> {
+#[derive(PartialEq, Copy, Debug)]
+pub enum LineBreak {
+    Generic,
+    LineSeparator,
+    ParagraphSeparator
+}
+
+impl <'a>Iterator for Tokenizer<'a> {
     
     type Item = Token;
 
@@ -63,14 +178,14 @@ mod test {
     use std::io::MemReader;
     use std::vec;
 
-    use super::{Scanner, Token};
+    use super::{Tokenizer, Token};
 
     #[test]
     fn test_scanner() {
         let stream = "[";
         let mut reader = MemReader::new(stream.bytes().collect());
-        let mut scanner = Scanner::new(Box::new(reader));
-        let tokens: Vec<Token> = scanner.collect();
+        let mut tokenizer = Tokenizer::new(Box::new(reader));
+        let tokens: Vec<Token> = tokenizer.collect();
         let expected = vec!(Token::SequenceStart, Token::Eof);
         assert!(tokens == expected);
     }
